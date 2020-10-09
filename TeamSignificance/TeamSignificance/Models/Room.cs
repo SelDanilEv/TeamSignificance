d700289a476de1d663893e80806b8924d3ba8fdf
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2020-10-09 23:23:23 +0300

    feat: add list of rooms, create models, make controller, optimize imports

---
+++ TeamSignificance/TeamSignificance/Models/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeamSignificance.Models
+{
+    public class Room
+    {
+        public Room()
+        {
+            Users = new List<User>();
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+
+        public int RoomSize => Users.Count;
+
+        public int GetCode => Math.Abs(Id.GetHashCode()+Name.GetHashCode()+Users.GetHashCode());
+    }
+}
---
+++ TeamSignificance/TeamSignificance/Models/Storage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamSignificance.Models
+{
+    public class Storage
+    {
+        private Storage()
+        {
+            Rooms = new List<Room>();
+        }
+
+        private static Storage _storage;
+
+        public static Storage GetStorage()
+        {
+            if (_storage == null)
+            {
+                _storage = new Storage();
+            }
+            return _storage;
+        }
+
+        public List<Room> Rooms { get; set; }
+    }
+}
